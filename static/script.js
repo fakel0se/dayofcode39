4f2d86ddc9ed17771842bfb7b43cb92f3da552f8
Ilya Gubanov <il07ya96@mail.ru>
2017-05-29 00:29:35 +0800

    win

--- static/script.js
+++ static/script.js
@@ -1,22 +1,18 @@
 var socket = io();
 
-var stage;
 var p = "px";
 
 //var canvas = document.getElementById("canvas");
-stage = new createjs.Stage("canvas");
-<<<<<<< HEAD
+var stage = new createjs.Stage("canvas");
 
-//var gridOX = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
 var gridOX = [
 	[-1, -1, -1],
 	[-1, -1, -1],
 	[-1, -1, -1]
 ];
+
 var figure = 0;
 
-=======
->>>>>>> sanya/master
 var data = {
 	images: ["/static/xo.png"],
 	frames: {width:150, height:146},
@@ -25,39 +21,13 @@ var data = {
 		circle: 1
 	}
 };
+
 var spriteSheet = new createjs.SpriteSheet(data);
-<<<<<<< HEAD
-	
-console.log('hi!');
-=======
+
 stage.on("stagemousedown", CreateXO);
 
-//var gridOX = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
-var gridOX = [
-	[-1, -1, -1],
-	[-1, -1, -1],
-	[-1, -1, -1]
-];
+Restart();
 
-var figure = 'circle';
-var count = 0;
-	
-console.log('New Game');
->>>>>>> sanya/master
-// Рисуем сетку
-var grid = new createjs.Shape();
-grid.graphics.beginFill("black").drawRect(parseFloat(document.getElementById("canvas").width) / 3, 0, 2, parseFloat(document.getElementById("canvas").height));
-grid.graphics.beginFill("black").drawRect(parseFloat(document.getElementById("canvas").width) / 3 * 2, 0, 2, parseFloat(document.getElementById("canvas").height));
-grid.graphics.beginFill("black").drawRect(0, parseFloat(document.getElementById("canvas").height) / 3, parseFloat(document.getElementById("canvas").width), 2);
-grid.graphics.beginFill("black").drawRect(0, parseFloat(document.getElementById("canvas").height) / 3 * 2, parseFloat(document.getElementById("canvas").width), 2);
-stage.addChild(grid);
-stage.update();
-
-<<<<<<< HEAD
-stage.on("stagemousedown", CreateXO);
-
-=======
->>>>>>> sanya/master
 function CreateXO(evt) { //Рисуем крестики-нолики
 	
 	//console.log('hi!');
@@ -66,46 +36,7 @@ function CreateXO(evt) { //Рисуем крестики-нолики
 	let X = 300 * column + 150;
 	let Y = 300 * line + 150;
 	//console.log('line = ' + line + ' column = ' + column);
-<<<<<<< HEAD
-	
-	
-	if (figure == 1) {
-		if (gridOX[line][column] == -1) {
-			console.log('Create Cross in X = ' + Math.floor(evt.stageX) + ' Y = ' + Math.floor(evt.stageY));
-			let cross = new createjs.Sprite(spriteSheet, "cross");
-			cross.regX = 75;
-			cross.regY = 75;
-			cross.x = X;
-			cross.y = Y;		
-			stage.addChild(cross);
-			stage.update();
-			figure = 0;
-			gridOX[line][column] = 0;
-		} else {
-				console.log('Figure already exists!');
-				alert('Figure already exists!');
-		}
-	} else {
-			if (gridOX[line][column] == -1) {
-				console.log('Create Circle in X = ' + Math.floor(evt.stageX) + ' Y = ' + Math.floor(evt.stageY));
 
-				let circle = new createjs.Sprite(spriteSheet, "circle");
-				circle.regX = 75;
-				circle.regY = 75;
-				circle.x = X;
-				circle.y = Y;		
-				stage.addChild(circle);
-				stage.update();
-				figure = 1;
-				gridOX[line][column] = 1;
-		} else {
-				console.log('Figure already exists!');
-				alert('Figure already exists!');
-		}
-	}
-};
-=======
-	let kas = gridOX[line][column];
 	if (gridOX[line][column] == -1) {
 		console.log('Create ' + figure + ' in X = ' + Math.floor(evt.stageX) + ' Y = ' + Math.floor(evt.stageY));
 		let fig = new createjs.Sprite(spriteSheet, figure);
@@ -194,7 +125,8 @@ function Restart() {
 		[-1, -1, -1],
 		[-1, -1, -1],
 		[-1, -1, -1]
-	];
+	];	
+	figure = 'circle';
 	count = 0;
 	console.log('New Game');
 	// Рисуем сетку
@@ -208,4 +140,3 @@ function Restart() {
 	stage.update();
 	// Неплохо было бы реализовать подсчет очков и вывод их на экран
 }
->>>>>>> sanya/master
